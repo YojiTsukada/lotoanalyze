72000b5d33b9af5da3f66978f5e8e24b1cf14ec5
Yoji Tsukada <y.tsukada@thinknetworks.jp>
2019-01-23 21:13:55 +0900

    Add to check repeat number & to check my number

--- analyze.py
+++ analyze.py
@@ -4,16 +4,43 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 %matplotlib inline
 
-# Read CSV
-csv_data  = pd.read_csv('./csv/loto6.csv',encoding='cp932')
+# Read Result CSV
+res_csv  = pd.read_csv('./csv/loto6.csv',encoding='cp932')
 
 # Change Colmuns
-csv_data.columns = ['no', 'date', '1','2','3','4','5','6','bounus','Hit1','Hit2','Hit3','Hit4','Hit5','1st Prize','2nd prize','3rd prize','4th prize','5th prize','carry over']
+res_csv.columns = ['no', 'date', '1','2','3','4','5','6','bounus','Hit1','Hit2','Hit3','Hit4','Hit5','1st Prize','2nd prize','3rd prize','4th prize','5th prize','carry over']
 
 # Extract 1-6 Count
-listdata = csv_data.loc[:,['1','2','3','4','5','6']]
+listdata = res_csv.loc[:,['no','1','2','3','4','5','6']]
 
-listdata
+# To change list
+array = listdata.values
+
+
+# check repeat Number
+repeat = []
+for x in array:
+    for y in array:
+        if (x[0] == y[0]):
+            break
+        if (x[1:] == y[1:]).all():
+            repeat.append(x[1:])
+            print(repeat)
+
+
+# Read My Number
+my_number = pd.read_csv('./csv/my_number.csv')
+my_number_array = my_number.values
+
+# Comparison
+matched_list = []
+for i in array:
+    for n in my_number_array:
+        if (i[:1] == n).all():
+            matched_list.append(i)
+
+# print result
+print(matched_list)
 
 # merged columns
 count = pd.melt(listdata)
@@ -24,8 +51,5 @@ count.columns = ["position","all_no"]
 # show data count (and sort)
 group = count.groupby('all_no').count().sort_values('position', ascending=False)
 
-
-group
-
-#df = pd.DataFrame({'val': np.random.rand(300)})
-#graph = group.plot(kind='bar', align='center')
+# show
+print(group)
